d762424efce87d93cbd3d66d281aa226e88b5868
uotani <s.uotani.zetakansu@gmail.com>
2019-01-12 12:24:27 +0000

    add owner admin model

---
+++ db/migrate/20190112122030_create_admins.rb
@@ -0,0 +1,11 @@
+class CreateAdmins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :admins do |t|
+      t.string :name
+      t.string :account
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end

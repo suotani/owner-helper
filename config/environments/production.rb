86c588670bcb1d987a308f957267cb0fc0097047
uotani <s.uotani.zetakansu@gmail.com>
2019-01-16 15:48:55 +0000

    change time zone

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.time_zone = 'Tokyo'
+  config.active_record.default_timezone = :local
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')

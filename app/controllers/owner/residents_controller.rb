c0bc6061f89aedfa4198810853cadcab7ef0d1c6
uotani <s.uotani.zetakansu@gmail.com>
2019-01-22 11:27:10 +0000

    little change

--- app/controllers/owner/residents_controller.rb
+++ app/controllers/owner/residents_controller.rb
@@ -1,7 +1,6 @@
 class Owner::ResidentsController < OwnerController
 
   def index
-    house_ids = @owner.houses.ids
-    @residents = Resident.joins(room: :house).merge(House.where(id: house_ids)).uniq
+    @residents = Resident.joins(room: :house).merge(House.where(id: @owner.houses.ids)).uniq
   end
 end

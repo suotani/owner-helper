ec87cc725d42af43fee8947c292664b8c7162a07
uotani <s.uotani.zetakansu@gmail.com>
2019-01-23 02:48:25 +0000

    unuse es6 syntax

--- app/assets/javascripts/resident/houses/new.js
+++ app/assets/javascripts/resident/houses/new.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(".house_name").on("click",function(){
-      const house_id = $(this).data("id");
-      const house_name = $(this).data("name");
+      var house_id = $(this).data("id");
+      var house_name = $(this).data("name");
       $("#house_name").val(house_name);
       $("#house_id").val(house_id);
     });

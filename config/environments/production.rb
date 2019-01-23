f61c4bf25922b5a1c84233c1dd8edb8d75f93318
uotani <s.uotani.zetakansu@gmail.com>
2019-01-23 15:53:51 +0000

    change mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'irent.test.com',
+    :domain         => 'mg.irenttest.work',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp

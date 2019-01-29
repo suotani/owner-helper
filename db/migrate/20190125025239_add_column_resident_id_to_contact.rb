36585c4aa8e1828f550bb2cd6f00b5cce1db5c72
uotani <s.uotani.zetakansu@gmail.com>
2019-01-29 02:47:25 +0000

    fix migration

--- db/migrate/20190125025239_add_column_resident_id_to_contact.rb
+++ db/migrate/20190125025239_add_column_resident_id_to_contact.rb
@@ -1,6 +1,5 @@
 class AddColumnResidentIdToContact < ActiveRecord::Migration[5.2]
   def change
     add_column :contacts, :resident_id, :integer
-    remove_column :posts, :room_id, :integer
   end
 end

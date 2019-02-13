e4217fe0e0b5f752dec596b8c46d84668c958478
uotani <s.uotani.zetakansu@gmail.com>
2019-02-13 00:55:56 +0000

    add asociation

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   belongs_to :house
   belongs_to :resident, optional: true
   has_many :contacts
+  belongs_to :contact
   
   validates :number, presence: true, uniqueness: {scope: :house_id}
 end

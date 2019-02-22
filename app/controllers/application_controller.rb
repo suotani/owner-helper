5a606107d0383ba101159ea43c4fc441d27d9fcd
uotani <s.uotani.zetakansu@gmail.com>
2019-02-22 01:20:42 +0000

    add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    
+    rescue_from StandardError, with: :render_standard
+    
+      def render_standard(e = nil)
+        if e
+          logger.error e 
+          logger.error e.backtrace.join("\n") 
+        end
+    
+        render template: '500', status: 500, layout: 'application', content_type: 'text/html'
+      end
 end
